745ad484a2d4135bb568cca66646bb755031e82f
vizvamitra <vizvamitra@gmail.com>
2017-10-11 23:11:23 +0300

    update rspec to 3.6, replace webrat with capybara

--- spec/helpers/uploader_tags_spec.rb
+++ spec/helpers/uploader_tags_spec.rb
@@ -3,13 +3,13 @@ require "spec_helper"
 describe Uploadcare::Rails::ActionView::UploaderTags do
   it "should include uploader tag for name" do
     tag = helper.uploadcare_uploader_tag :file
-    tag.should be_kind_of(String)
-    tag.should have_selector("input", :type =>"hidden", :role =>"uploadcare-uploader", "data-path-value" => "true")
+    expect(tag).to be_kind_of(String)
+    expect(tag).to have_selector('input[type="hidden"][role="uploadcare-uploader"][data-path-value="true"]', visible: :all)
   end
 
   it "should include uploader field for object" do
     tag = helper.uploadcare_uploader_field :post, :file
-    tag.should be_kind_of(String)
-    tag.should have_selector("input", :type =>"hidden", :role =>"uploadcare-uploader", "data-path-value" => "true")
+    expect(tag).to be_kind_of(String)
+    expect(tag).to have_selector('input[type="hidden"][role="uploadcare-uploader"][data-path-value="true"]', visible: :all)
   end
 end
